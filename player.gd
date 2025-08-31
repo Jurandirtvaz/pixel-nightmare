4581aec334f31ddb0513f9d90781e1509decf990
Jurandirtvaz <jurandirtvaz28@gmail.com>
2025-08-30 21:53:49 -0300

    Criação das Mecânicas do Player 30/08
    Foi criado a movimentação básica do player e tela de Game Over.

--- player.gd
+++ player.gd
@@ -1,24 +1,38 @@
 extends CharacterBody2D
 
-@export var speed = 150.0          # Velocidade
-@export var jump_velocity = -300.0 # Força do pulo
-@export var acceleration = 800.0   # Quão rápido o personagem atinge a velocidade máxima
-@export var friction = 1000.0      # Quão rápido o personagem para ao soltar a tecla de movimento
+@export var speed: float = 300.0
+@export var acceleration: float = 1500.0 
+@export var friction: float = 1500.0
+@export var max_hp: int = 3 #Vida máxima
+var hp: int = max_hp #Vida atual
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _physics_process(delta: float) -> void:
+	var input_vector := Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
-func _physics_process(delta):
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	input_vector = input_vector.normalized()
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-		
-	var direction = Input.get_axis("move_left", "move_right")
 
-	if direction:
-		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * speed, acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, friction * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 
 	move_and_slide()
+	
+func tomar_dano(amount: int) -> void:
+	hp -= amount
+	if hp < 0 :
+		hp = 0
+	print("HP atual:", hp)
+	if hp == 0:
+		morrer()
+
+func morrer() -> void:
+	velocity = Vector2.ZERO
+	print("GAME OVER")
+	#Ainda vou implementar:         (NETO)
+	#Adicionar uma troca de cena
+	#Fazer uma animacao de morte
+	#Mostrar um hud de game over
