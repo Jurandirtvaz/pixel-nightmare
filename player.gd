b6909c3cb7baadb4929f75975fd4ba04ce8643bb
Jurandirtvaz <jurandirtvaz28@gmail.com>
2025-09-08 23:03:07 -0300

    {feat} Mecânicas de movimentação do Player
    -Implementado por placeholder mecânicas para saber se o player está andando, parado ou morto.
    -Melhoria na cena de GameOver como uma overlay

--- player.gd
+++ player.gd
@@ -6,8 +6,12 @@ extends CharacterBody2D
 @export var vidas_maximas: int = 3 #Vida máxima
 var vidas: int = vidas_maximas #Vida atual
 var hud : Node
+@onready var placeholder: ColorRect = $ColorRect
+var esta_morto : bool = false
 
 func _physics_process(delta: float) -> void:
+	if esta_morto:
+		return
 	var input_vector := Vector2.ZERO
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
@@ -17,9 +21,10 @@ func _physics_process(delta: float) -> void:
 
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * speed, acceleration * delta)
+		_sprite_andando()
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-
+		_sprite_parado()
 	move_and_slide()
 	
 func tomar_dano() -> void:
@@ -37,16 +42,31 @@ func tomar_dano() -> void:
 		morrer()
 
 func morrer() -> void:
+	esta_morto = true
 	velocity = Vector2.ZERO
+	_sprite_morto()
 	print("GAME OVER")
 	await get_tree().create_timer(0.3).timeout
-	get_tree().change_scene_to_file("res://Cenas/game_over.tscn")
+	
+	#Adicionando o game-over como overlay
+	var overlay = preload("res://Cenas/game_over.tscn").instantiate()
+	get_tree().current_scene.add_child(overlay)
+	get_tree().paused = true
 	
 #funcao para testar o dano e o hud das vidas
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("teste_dano"):
 		tomar_dano()
 		
 func _ready() -> void:
 	hud = get_tree().get_first_node_in_group("hud")
 	print("HUD encontrado? ", hud != null)
+
+func _sprite_parado() -> void:
+	placeholder.color = Color(1, 1, 1, 1) #Azul ele esta parado
+
+func _sprite_andando() -> void:
+	placeholder.color = Color(0, 1, 0, 1) #Verde esta andando
+
+func _sprite_morto() -> void:
+	placeholder.color = Color(1, 0, 0, 1) #Vermelho esta morto
